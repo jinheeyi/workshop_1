f337d76f20158d45b931509fe657e04d8fac3aae
jinheeyi <jyi111@aucklanduni.ac.nz>
2025-01-16 17:04:19 +1300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,27 @@
 function setup() {
-  createCanvas(400, 400);
-}
+    createCanvas(800, 600);
+    noStroke();
+  }
+  
+  function draw() {
+  
+    fill(0, 10); 
+    rect(0, 0, width, height);
+  
+    let size = random(20, 100); 
+    let x = random(width); 
+    let y = random(height);
 
-function draw() {
-  background(220);
-}
+    fill(random(255), random(255), random(255), 150); 
+    if (random() < 0.5) {
+      ellipse(x, y, size, size);
+    } else {
+      rect(x, y, size, size);
+    }
+  
+   
+    if (mouseIsPressed) {
+      fill(random(255), random(255), random(255), 200);
+      ellipse(mouseX + random(-10, 10), mouseY + random(-10, 10), 10, 10);
+    }
+}  
